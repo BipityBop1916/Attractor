61b6bfb3b27d92649671127e7b9e037e1e598a47
Danil Jumashev <danil.jumash@gmail.com>
2025-06-06 16:51:33 +0600

    #27 - New hw started. Removed old hw.

---
+++ hw27/ex1/ArrayTool.cs
@@ -0,0 +1,14 @@
+﻿namespace hw27;
+
+public class ArrayTool<T> where T : IComparable<T>
+{
+    static T[] Sort(T[] array)
+    {
+        
+    }
+
+    static T[] Reverse(T[] array)
+    {
+        
+    }
+}

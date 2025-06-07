969000bcfea8127a32d0bc7fb4dbc0d3b409c9d3
Danil Jumashev <danil.jumash@gmail.com>
2025-06-08 02:24:40 +0600

    #28 - Completed main exercise. Begin extra exercise.
    1. Added generic class LinkedNode, representing one value in MyLinkedList.
    
    2. Added generic class MyLinkedList, emulating C#'s inbuilt LinkedList.
    
    3. Begin extra exercise. Estimated 45m-1h. Current time is 2:23AM. Estimated end is 3AM-3:15AM.

---
+++ hw27/LinkedNode.cs
@@ -0,0 +1,52 @@
+﻿using System.Diagnostics.Contracts;
+
+namespace hw27;
+
+public class MyLinkedList<T>
+{
+    private LinkedNode<T> _first;
+    private LinkedNode<T> _last;
+    private int _count;
+    
+    public LinkedNode<T> First => _first;
+    public LinkedNode<T> Last => _last;
+    public int Count => _count;
+
+    public MyLinkedList()
+    {
+        _first = null;
+        _last = null;
+        _count = 0;
+    }
+
+    public void Add(T value)
+    {
+        LinkedNode<T> newNode = new LinkedNode<T>(value);
+        _count++;
+
+        if (_first == null)
+        {
+            _first = _last = newNode;
+        }
+        else
+        {
+            _last.Next = newNode;
+            newNode.Previous = _last;
+            _last = newNode;
+        }
+    }
+}
+
+public class LinkedNode<T>
+{
+    public T Value { get; set; }
+    public LinkedNode<T> Next { get; set; }
+    public LinkedNode<T> Previous { get; set; }
+
+    public LinkedNode(T value)
+    {
+        Value = value;
+        Next = null;
+        Previous = null;
+    }
+}

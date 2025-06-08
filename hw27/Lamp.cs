67b0e48ae85203b9ad0e61fa2fd8a0ca18ad26cd
Danil Jumashev <danil.jumash@gmail.com>
2025-06-08 06:33:59 +0600

    #30 - Begun ex1. Deleted hw28.
    Current time: 6:33AM. ETA: 1h. 7:33AM. View actual finish time in future commit history.

---
+++ hw27/Lamp.cs
@@ -0,0 +1,27 @@
+﻿namespace hw27;
+
+public class Lamp
+{
+    public bool TurnedOn { get; set; }
+
+    public string GetLampState()
+    {
+        
+    }
+}
+
+public class Cat
+{
+    public string GetCatState()
+    {
+        
+    }
+}
+
+public class Man
+{
+    public string GetManState()
+    {
+        
+    }
+}

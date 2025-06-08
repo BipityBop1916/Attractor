2dddc94d0aa2f86684318d56b0193bff44506f2a
Danil Jumashev <danil.jumash@gmail.com>
2025-06-08 09:47:47 +0600

    #33 - Completed ex1.

--- hw27/Program.cs
+++ hw27/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
+        Console.WriteLine("Enter string: ");
+        string? input = Console.ReadLine().MyTrim();
         
+        if (input.MyIsNullOrWhiteSpace()) Console.WriteLine("String is empty");
+        else
+        {
+            Console.WriteLine("Enter choice: (1, 2) \n1.Turn uppercase \n2.Turn lowercase");
+            string? choice = Console.ReadLine().MyTrim();
+
+            switch (choice)
+            {
+                case "1":
+                    Console.WriteLine(input.MyToUpper());
+                    break;
+            
+                case "2":
+                    Console.WriteLine(input.MyToLower());
+                    break;
+                
+                default:
+                    Console.WriteLine("Invalid choice");
+                    break;
+            }
+        }
     }
 }

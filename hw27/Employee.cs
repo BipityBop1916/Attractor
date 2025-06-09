dcb63f8f7a63e22e7b3b046dbad4cc9adf4368ca
Danil Jumashev <danil.jumash@gmail.com>
2025-06-09 17:27:06 +0600

    #34 - Completed ex1. 5:26PM.

---
+++ hw27/Employee.cs
@@ -0,0 +1,23 @@
+﻿namespace hw27;
+
+public class Employee : IComparable<Employee>
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public int Birthyear { get; set; }
+    public int Salary { get; set; }
+
+    public Employee(string name, string surname, int birthyear, int salary)
+    {
+        Name = name;
+        Surname = surname;
+        Birthyear = birthyear;
+        Salary = salary;
+    }
+
+    public int CompareTo(Employee? other)
+    {
+        if (other == null) return 1;
+        return String.Compare(Name, other.Name);
+    }
+}

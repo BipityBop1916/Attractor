0feae4e8acd4c1e5ee2f57204bbe0d4f6bce48ed
Danil Jumashev <danil.jumash@gmail.com>
2025-06-09 18:29:15 +0600

    #34 - Completed ex2.

--- hw27/Employee.cs
+++ hw27/Employee.cs
@@ -21,3 +21,37 @@ public class Employee : IComparable<Employee>
         return String.Compare(Name, other.Name);
     }
 }
+
+class EmployeeContainer : IEnumerable<Employee>
+{
+    private List<Employee> employees = new List<Employee>();
+    
+    public List<Employee> Employees { get { return employees; } }
+
+    public IEnumerator<Employee> GetEnumerator()
+    {
+        return new EmployeeContainerEnumerator(this);
+    }
+    
+    System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator() => GetEnumerator();
+}
+
+class EmployeeContainerEnumerator : IEnumerator<Employee>
+{
+    private EmployeeContainer _container;
+    private int _index = -1;
+    
+    public EmployeeContainerEnumerator(EmployeeContainer container) => _container = container;
+    
+    public Employee Current => _container.Employees[_index];
+    object System.Collections.IEnumerator.Current => Current;
+
+    public bool MoveNext()
+    {
+        return ++_index < _container.Employees.Count;
+    }
+    
+    public void Reset() => _index = -1;
+    
+    public void Dispose() { }
+}

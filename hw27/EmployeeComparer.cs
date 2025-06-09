69b1fd5a7b262265f6480615617837684cabcc39
Danil Jumashev <danil.jumash@gmail.com>
2025-06-09 17:27:43 +0600

    #30 - misclicked and didnt push comparer

---
+++ hw27/EmployeeComparer.cs
@@ -0,0 +1,48 @@
+﻿namespace hw27;
+
+public class EmployeeComparer : IComparer<Employee>
+{
+    private string _type;
+
+    public EmployeeComparer(string type)
+    {
+        _type = type;
+    }
+    
+    public int Compare(Employee? x, Employee? y)
+    {
+        switch (_type)
+        {
+            case "name":
+                return String.Compare(x.Name, y.Name);
+            
+            case "surname":
+                return String.Compare(x.Surname, y.Surname);
+            
+            case "birthyear":
+                if (x.Birthyear > y.Birthyear)
+
+                    return 1;
+
+                if (x.Birthyear < y.Birthyear)
+
+                    return -1;
+
+                return 0;
+            
+            case "salary":
+                if (x.Salary > y.Salary)
+
+                    return -1;
+
+                if (x.Salary < y.Salary)
+
+                    return 1;
+
+                return 0;
+            
+            default:
+                return 0;
+        }
+    }
+}
